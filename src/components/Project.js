6dd450737c8279cff95e71bf605e5f8172d5fbec
Ashish Saini <sainiashish08@gmail.com>
2021-10-23 00:08:01 +0530

    🎨 Improve project component structure.
    Signed-off-by: Ashish Saini <sainiashish08@gmail.com>

---
+++ src/components/Project.js
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import styled from "styled-components";
+
+const Project = ({ title, image, description, link }) => {
+	return (
+		<AnchorStyle href={link} target="_blank" rel="noreferrer">
+			<ProjectStyle>
+				<StyledImg image={getImage(image)} alt={title} />
+				<div className="project-details-container">
+					<div className="title">{title}</div>
+					<div className="description">{description}</div>
+				</div>
+			</ProjectStyle>
+		</AnchorStyle>
+	);
+};
+
+const StyledImg = styled(GatsbyImage)`
+	max-width: 6rem;
+	min-height: 6rem;
+	border-radius: 8px;
+	margin: 16px;
+`;
+
+const AnchorStyle = styled.a`
+	text-decoration: none;
+`;
+
+const ProjectStyle = styled.div`
+	border: solid 1px var(--color-text);
+	border-radius: 4px;
+	height: 8rem;
+	display: flex;
+	margin: 16px 0px;
+	.project-details-container {
+		margin: 8px;
+		overflow: auto;
+	}
+	.title {
+		margin: 8px 0;
+		color: var(--color-text);
+	}
+	.description {
+		font-size: 0.8em;
+		color: var(--color-gray700);
+	}
+`;
+
+Project.propTypes = {
+	title: PropTypes.string.isRequired,
+	image: PropTypes.any.isRequired,
+	description: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+};
+
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,65 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 import useSound from "use-sound";
 import { useIntl, Link } from "gatsby-plugin-react-intl";
 
 import click_sound from "../sounds/click.mp3";
 import { useSoundEffect } from "./SoundEffectHook";
-
-const Project = ({ title, image, description, link }) => {
-	return (
-		<AnchorStyle href={link} target="_blank" rel="noreferrer">
-			<ProjectStyle>
-				<StyledImg image={getImage(image)} alt={title} />
-				<div className="project-details-container">
-					<div className="title">{title}</div>
-					<div className="description">{description}</div>
-				</div>
-			</ProjectStyle>
-		</AnchorStyle>
-	);
-};
-
-const StyledImg = styled(GatsbyImage)`
-	max-width: 6rem;
-	min-height: 6rem;
-	border-radius: 8px;
-	margin: 16px;
-`;
-
-const AnchorStyle = styled.a`
-	text-decoration: none;
-`;
-
-const ProjectStyle = styled.div`
-	border: solid 1px var(--color-text);
-	border-radius: 4px;
-	height: 8rem;
-	display: flex;
-	margin: 16px 0px;
-	.project-details-container {
-		margin: 8px;
-		overflow: auto;
-	}
-	.title {
-		margin: 8px 0;
-		color: var(--color-text);
-	}
-	.description {
-		font-size: 0.8em;
-		color: var(--color-gray700);
-	}
-`;
-
-Project.propTypes = {
-	title: PropTypes.string.isRequired,
-	image: PropTypes.any.isRequired,
-	description: PropTypes.string.isRequired,
-	link: PropTypes.string.isRequired,
-};
+import Project from "./Project";
 
 export const fluidImage = graphql`
 	fragment fluidImage on File {
@@ -68,6 +16,67 @@ export const fluidImage = graphql`
 		}
 	}
 `;
+
+const getProjects = intl => {
+	return [
+		{
+			id: "ignou_datesheet",
+			link: "https://gc.ashishsaini.me/projects/ignou-datesheet/index.html",
+			title: intl.formatMessage({ id: "project_ignou_datesheet_ttl" }),
+			image: "ignou.jpg",
+			description: intl.formatMessage({
+				id: "project_ignou_datesheet_desc",
+			}),
+		},
+		{
+			id: "tmpmail",
+			link: "https://github.com/meashishsaini/tmpmail-python",
+			title: intl.formatMessage({ id: "project_tmpmail_ttl" }),
+			image: "python.png",
+			description: intl.formatMessage({ id: "project_tmpmail_desc" }),
+		},
+		{
+			id: "captcha",
+			link: "https://github.com/meashishsaini/captcha-solver",
+			title: intl.formatMessage({ id: "project_captcha_solver_ttl" }),
+			image: "captcha-solver.jpg",
+			description: intl.formatMessage({
+				id: "project_captcha_solver_desc",
+			}),
+		},
+		{
+			id: "bsnl",
+			link: "https://github.com/meashishsaini/bsnl",
+			title: intl.formatMessage({ id: "project_bsnl_scripts_ttl" }),
+			image: "python.png",
+			description: intl.formatMessage({
+				id: "project_bsnl_scripts_desc",
+			}),
+		},
+		{
+			id: "datesheet_extract",
+			link: "https://github.com/meashishsaini/ignou-datesheet-extract",
+			title: intl.formatMessage({ id: "project_ignou_dst_ext_ttl" }),
+			image: "python.png",
+			description: intl.formatMessage({
+				id: "project_ignou_dst_ext_desc",
+			}),
+		},
+	];
+};
+
+/**
+ * @description Get a single image data from nodes.
+ * @param {{allFile: {edges: Array.<{node: {ext: string, name: string, childImageSharp: Object}}>}}} all
+ * @param {string} name
+ * @returns {ext: string, name: string, childImageSharp: Object}}
+ */
+const getImageNodeByName = (all, name) => {
+	return all.allFile.edges.find(
+		edge => edge.node.name + edge.node.ext === name
+	).node;
+};
+
 const Projects = ({ wholePage }) => {
 	const [showAll, setShowAll] = React.useState(false);
 	const [click] = useSound(click_sound);
@@ -77,68 +86,27 @@ const Projects = ({ wholePage }) => {
 
 	React.useEffect(() => setClient(true), []);
 
-	const data = useStaticQuery(graphql`
-		{
-			ignouDateSheetImage: file(
-				relativePath: { eq: "projects/ignou.jpg" }
-			) {
-				...fluidImage
-			}
-			captchaSolver: file(
-				relativePath: { eq: "projects/captcha-solver.jpg" }
-			) {
-				...fluidImage
-			}
-			python: file(relativePath: { eq: "projects/python.png" }) {
-				...fluidImage
+	const projects = getProjects(intl);
+
+	const projectImages = useStaticQuery(graphql`
+		query projectImages {
+			allFile(filter: { dir: { glob: "**/images/projects" } }) {
+				edges {
+					node {
+						name
+						childImageSharp {
+							gatsbyImageData(
+								layout: CONSTRAINED
+								placeholder: BLURRED
+							)
+						}
+						ext
+					}
+				}
 			}
 		}
 	`);
-	const projects = [
-		<Project
-			key={1}
-			link="https://gc.ashishsaini.me/projects/ignou-datesheet/index.html"
-			title={intl.formatMessage({ id: "project_ignou_datesheet_ttl" })}
-			image={data.ignouDateSheetImage}
-			description={intl.formatMessage({
-				id: "project_ignou_datesheet_desc",
-			})}
-		/>,
-		<Project
-			key={2}
-			link="https://github.com/meashishsaini/tmpmail-python"
-			title={intl.formatMessage({ id: "project_tmpmail_ttl" })}
-			image={data.python}
-			description={intl.formatMessage({ id: "project_tmpmail_desc" })}
-		/>,
-		<Project
-			key={3}
-			link="https://github.com/meashishsaini/captcha-solver"
-			title={intl.formatMessage({ id: "project_captcha_solver_ttl" })}
-			image={data.captchaSolver}
-			description={intl.formatMessage({
-				id: "project_captcha_solver_desc",
-			})}
-		/>,
-		<Project
-			key={4}
-			link="https://github.com/meashishsaini/bsnl"
-			title={intl.formatMessage({ id: "project_bsnl_scripts_ttl" })}
-			image={data.python}
-			description={intl.formatMessage({
-				id: "project_bsnl_scripts_desc",
-			})}
-		/>,
-		<Project
-			key={5}
-			link="https://github.com/meashishsaini/ignou-datesheet-extract"
-			title={intl.formatMessage({ id: "project_ignou_dst_ext_ttl" })}
-			image={data.python}
-			description={intl.formatMessage({
-				id: "project_ignou_dst_ext_desc",
-			})}
-		/>,
-	];
+
 	const showMessage = showAll
 		? intl.formatMessage({ id: "show_less" })
 		: intl.formatMessage({ id: "show_all" });
@@ -150,8 +118,15 @@ const Projects = ({ wholePage }) => {
 				{intl.formatMessage({ id: "projects_introduction" })}
 			</article>
 			{/* Show only two projects by default. */}
-			{projects.slice(0, 2)}
-			{(showAll || wholePage) && projects.slice(2, projects.length)}
+			{projects
+				.slice(0, showAll || wholePage ? projects.length : 2)
+				.map(project => (
+					<Project
+						key={project.id}
+						{...project}
+						image={getImageNodeByName(projectImages, project.image)}
+					/>
+				))}
 			{!wholePage &&
 				/* Render a link during SSR else render button.*/
 				(isClient ? (
